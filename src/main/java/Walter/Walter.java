a6843cf58dd6ca433f227e520b183f1898fb585e
Dominik Galbavý <anders142536@gmail.com>
2019-10-17 19:42:12 +0200

    added dependencies to get add the logger and reflection changed prints in Walter

--- src/main/java/Walter/Walter.java
+++ src/main/java/Walter/Walter.java
@@ -1,11 +1,11 @@
 package Walter;
 
 import net.dv8tion.jda.api.*;
+import org.slf4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.net.URLDecoder;
 
 public class Walter {
 
@@ -23,12 +23,13 @@ public class Walter {
             System.out.println("token was not found");
         }
 
-        System.out.println("path: " + path +
-                            "\ntoken: >" + token + "<");
-        JDA api = new JDABuilder(AccountType.BOT)
+        System.out.println("token: >" + token + "<");
+//        Logger logger = ;
+        JDA api = new JDABuilder()
                 .setToken(token)
 //                .addEventListeners(new Listener())
                 .build();
+
     }
 
     //TODO: read the token from a file
