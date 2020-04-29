f15d88a219d06f251ab6bed7e6c11558e1c206e0
Dominik Galbavý <anders142536@gmail.com>
2020-04-29 21:44:11 +0200

    edited patch notes for next release

--- src/main/java/Walter/commands/patch.java
+++ src/main/java/Walter/commands/patch.java
@@ -37,27 +37,10 @@ public class patch extends Command {
         String toSend = "__**Walter " + Walter.VERSION + "**__\n\n" +
                 "**New Features**\n" +
                 // enter new features between here..
-                item("implemented !patch command") +
-                item("implemented !roll command") +
-                item("implemented !playing command") +
-                item("implemented !watching command") +
-                item("implemented !listening command") +
-                item("messages sent into the news channel are now again being reposted by walter, nicely formatted") +
-                item("newly joined users will now automatically get the guest role assigned") +
-                item("in case a user does a mistake on using a command walter now automatically shows the help page of the command") +
-                note("the help pages are designed to explain **how** to use the command and the error messages should tell " +
-                        "you **what** you did wrong. until now the error messages told you both, which meant having almost " +
-                        "exactly the same text in two different places of the code. So this should make everything more " +
-                        "user friendly **and** easier to maintain. win-win!") +
-                item("guests are now announced differently") +
+                item("not yet implemented commands are now removed from !commands list") +
                 // ..and here
                 "\n**Bug Fixes**\n" +
                 // enter bugfixes between here..
-                item("fixed issue with help texts of patch command displaying the news channel name incorrectly") +
-                item("fixed issue with walter displaying english help text to german users and vice versa") +
-                item("clarified some unclear error messages") +
-                item("improved formatting of exception error messages") +
-                item("slight code refactor") +
                 // ..and here
                 "\nIn case you encounter any issues or wish for new features please contact <@!151010441043116032>";
 
